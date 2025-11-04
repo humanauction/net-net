b6cc831c9882162b153eeeb0f2f9b9ada997b24f
humanauction <t.a.harris@gmail.com>
2025-11-04 16:11:51 +0000

    Add: StatsPersistence: saveWindow: serialization, insertion of aggregated stats into SQLite

--- src/core/StatsPersistence.cpp
+++ src/core/StatsPersistence.cpp
@@ -29,7 +29,46 @@ StatsPersistence::StatsPersistence(const std::string& db_path)
 }
 
 void StatsPersistence::saveWindow(const AggregatedStats& stats) {
-    // TODO: Serialize stats and insert into SQLite
+    sqlite3* db;
+    if (sqlite3_open(db_path_.c_str(), &db) != SQLITE_OK) {
+        std::cerr << "Failed to open database: " << sqlite3_errmsg(db) << std::endl;
+        return;
+    }
+    sqlite3_exec(db, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
+
+    const char* insert_sql =
+        "INSERT INTO agg_stats(window_start, iface, protocol, src_ip, src_port, dst_ip, dst_port, pkts_c2s, pkts_s2c, bytes_c2s, bytes_s2c)";
+        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+    sqlite3_stmt* stmt;
+    if (sqlite3_prepare_v2(db, insert_sql, -1, &stmt, nullptr) != SQLITE_OK) {
+        std::cerr << "Failed to prepare statement: " << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
+        return;
+    }
+
+    for (const auto& kv : stats.flows) {
+        const FlowKey& key = kv.first;
+        const FlowStats& fs = kv.second;
+        sqlite3_bind_int64(stmt, 1, std::chrono::duration_cast<std::chrono::seconds>(stats.window_start.time_since_epoch()).count());
+        sqlite3_bind_text(stmt, 2, key.iface.c_str(), -1, SQLITE_TRANSIENT);
+        sqlite3_bind_int(stmt, 3, key.protocol);
+        sqlite3_bind_text(stmt, 4, key.src_ip.c_str(), -1, SQLITE_TRANSIENT);
+        sqlite3_bind_int(stmt, 5, key.src_port);
+        sqlite3_bind_text(stmt, 6, key.dst_ip.c_str(), -1, SQLITE_TRANSIENT);
+        sqlite3_bind_int(stmt, 7, key.dst_port);
+        sqlite3_bind_int(stmt, 8, fs.pkts_c2s);
+        sqlite3_bind_int(stmt, 9, fs.pkts_s2c);
+        sqlite3_bind_int(stmt, 10, fs.bytes_c2s);
+        sqlite3_bind_int(stmt, 11, fs.bytes_s2c);
+
+        if (sqlite3_step(stmt) != SQLITE_DONE) {
+            std::cerr << "Failed to execute statement: " << sqlite3_errmsg(db) << std::endl;
+        }
+        sqlite3_reset(stmt);
+    }
+    sqlite3_finalize(stmt);
+    sqlite3_exec(db, "END TRANSACTION;", nullptr, nullptr, nullptr);
+    sqlite3_close(db);
 }
 
 std::vector<AggregatedStats> StatsPersistence::loadHistory(size_t max_windows) {
