63783b562199fcaa1731804a04321d26f41bb493
humanauction <t.a.harris@gmail.com>
2025-12-02 16:01:27 +0000

    Add: UUID generation: remove platform-specific conditionals.

--- include/net-net/vendor/uuid_gen.cpp
+++ include/net-net/vendor/uuid_gen.cpp
@@ -1,14 +1,11 @@
 #include "uuid_gen.h"
-#ifdef __APPLE__
 #include <uuid/uuid.h>
-#endif
 #include <sstream>
 #include <stdexcept>
 
 namespace uuid_gen {
 
 std::string generate() {
-#ifdef __APPLE__
     uuid_t uuid;
     uuid_generate_random(uuid);
 
@@ -16,17 +13,6 @@ std::string generate() {
     uuid_unparse_lower(uuid, uuid_str);
 
     return std::string(uuid_str);
-#elif defined(__linux__)
-    uuid_t uuid;
-    uuid_generate_random(uuid);
-
-    char uuid_str[37];
-    uuid_unparse_lower(uuid, uuid_str);
-
-    return std::string(uuid_str);
-#else
-    throw std::runtime_error("UUID generation not supported on this platform");
-#endif
 }
 
 } // namespace uuid_gen
